1c9be9e1b8a69ed0445cbd93b73392c21661c66a
Jaiycex <juliaannglasser@gmail.com>
2023-08-06 11:10:06 -0700

    first version of corr function. Function provides intended result, but for some reason had issue with calling it using source() so had to run manually.

--- corr.R
+++ corr.R
@@ -1,26 +1,23 @@
-corr <- function(directory, threshold=0){
+function(directory, threshold = 0){
   
   # Combine all .csv files in the directory into one data frame 
-  # for selected monitor ids
   
-  numfiles <- length(list.files(directory, pattern="*.csv"))
-  full_names <- list.files(directory, full.names = TRUE)
-  dat <- data.frame()
-  for(i in numfiles){
-    dat <- rbind(dat, read.csv(full_names[i]))
+  files <- list.files(directory, pattern="*.csv", full.names = TRUE)
+  
+  # go through files and add to vector if exceeds threshold
+  corvec <- numeric()
+  for (i in 1:332) {
+    data <- na.omit(read.csv(files[i]))
+    nobs <- sum(complete.cases(data))
+    
+    
+    if (nobs > threshold){
+    
+    csulfate <- (data[,2])
+    cnitrate <- (data[,3])
+    
+    corvec <- c(corvec, cor(csulfate,cnitrate))
+    }
   }
-  
-  # subset dat to only include complete data rows
-  
-  completedata <- subset(dat, (!is.na(dat$sulfate)) & (!is.na(dat$nitrate)))
-  
-  # get vector of number of complete cases for each id
-  nobs <- c()
-  for(i in 1:length(numfiles)){
-    nobs[i] <- length(completedata$ID[completedata$ID == numfiles[i]])
-  }
-  
-  # make a logical vector for if nobs exceeds threshold
-  sel <- nobs >= threshold
-  monitors <- which(sel %in% TRUE)
+  return(corvec)
 }
